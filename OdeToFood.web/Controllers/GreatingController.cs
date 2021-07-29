3309634b74c19064c60ec20dc4aeb0794d803c06
Rafael Suarez <rafaelv_s@hotmail.com>
2021-07-29 10:33:41 -0300

    template bootstrap ace

---
+++ OdeToFood.web/Controllers/GreatingController.cs
@@ -0,0 +1,20 @@
+﻿using OdeToFood.web.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OdeToFood.web.Controllers
+{
+    public class GreatingController : Controller
+    {
+        // GET: Greating
+        public ActionResult Index()
+        {
+            var model = new GreatingViewModel() { Message = ConfigurationManager.AppSettings["message"]};
+            return View(model);
+        }
+    }
+}
---
+++ OdeToFood.web/Models/GreatingViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OdeToFood.web.Models
+{
+    public class GreatingViewModel
+    {
+        public string Message { get; set; }
+    }
+}
