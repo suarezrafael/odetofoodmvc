094e1d26373b578185809be99160f4e37ddb833b
Rafael Suarez <rafaelv_s@hotmail.com>
2021-07-30 15:13:15 -0300

    adicionado validação client-side e msg de sucesso ao salvar

--- OdeToFood.web/Controllers/RestaurantsController.cs
+++ OdeToFood.web/Controllers/RestaurantsController.cs
@@ -69,6 +69,7 @@ namespace OdeToFood.web.Controllers
             if (ModelState.IsValid)
             {
                 db.Update(restaurant);
+                TempData["Message"] = "You have save de restaurant";
                 return RedirectToAction("Details", new { id = restaurant.Id });
             }
             return View();
